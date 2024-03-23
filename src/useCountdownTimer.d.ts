78f309db1b0f1af2a25585a9b4df8b433b725222
Sourav Ojha <subhamojha9@gmail.com>
2024-03-23 19:51:13 +0530

    moved to roll-up

--- src/useCountdownTimer.d.ts
+++ src/useCountdownTimer.d.ts
@@ -1,11 +1,11 @@
 export type UseCountDownTimer = () => {
-    seconds: number;
-    start: (sec: number, callback: () => void) => void;
-    pause: () => void;
-    resume: () => void;
-    reset: () => void;
-    executeCallback: () => void;
-    clear: () => void;
-    status: string;
+  seconds: number;
+  start: (sec: number, callback: () => void) => void;
+  pause: () => void;
+  resume: () => void;
+  reset: () => void;
+  executeCallback: () => void;
+  clear: () => void;
+  status: string;
 };
 export declare const useCountDownTimer: UseCountDownTimer;
